55e239f4479f322dacab0aa1731f94ba31a1f05f
Florian Savouré <5079066-P0SlX@users.noreply.gitlab.com>
2021-03-18 17:48:45 +0100

    Add TVShows.ts

---
+++ src/app/tvshows/tvshows.ts
@@ -0,0 +1,18 @@
+// tslint:disable:variable-name
+export class TVShow {
+  constructor(
+    backdrop_path: string,
+    first_air_date: string,
+    genre_ids: number[],
+    id: number,
+    name: string,
+    origin_country: string[],
+    original_language: string,
+    original_name: string,
+    overview: string,
+    popularity: number,
+    poster_path: string,
+    vote_average: number,
+    vote_count: number
+  ) { }
+}

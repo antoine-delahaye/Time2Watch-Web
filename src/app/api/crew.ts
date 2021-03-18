f4a4c5e7f2417578a0d00bac644a56a87a089202
Florian Savouré <5079066-P0SlX@users.noreply.gitlab.com>
2021-03-18 19:04:30 +0100

    Add Crew object

---
+++ src/app/api/crew.ts
@@ -0,0 +1,17 @@
+// tslint:disable:variable-name
+export class Crew {
+  constructor(
+    adult: boolean,
+    gender: number,
+    id: number,
+    known_for_department: string,
+    name: string,
+    original_name: string,
+    popularity: number,
+    profile_path: string,
+    credit_id: string,
+    department: string,
+    job: string
+  ) {
+  }
+}

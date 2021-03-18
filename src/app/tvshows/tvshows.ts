0333e3e5b0f2922fd8afc947c8c57910ccb0dd3a
Florian Savouré <5079066-P0SlX@users.noreply.gitlab.com>
2021-03-18 17:49:10 +0100

    Add public to parameter TVShows.ts

--- src/app/tvshows/tvshows.ts
+++ src/app/tvshows/tvshows.ts
@@ -1,18 +1,18 @@
 // tslint:disable:variable-name
-export class TVShow {
+export class TVShows {
   constructor(
-    backdrop_path: string,
-    first_air_date: string,
-    genre_ids: number[],
-    id: number,
-    name: string,
-    origin_country: string[],
-    original_language: string,
-    original_name: string,
-    overview: string,
-    popularity: number,
-    poster_path: string,
-    vote_average: number,
-    vote_count: number
+    public backdrop_path: string,
+    public first_air_date: string,
+    public genre_ids: number[],
+    public id: number,
+    public name: string,
+    public origin_country: string[],
+    public original_language: string,
+    public original_name: string,
+    public overview: string,
+    public popularity: number,
+    public poster_path: string,
+    public vote_average: number,
+    public vote_count: number
   ) { }
 }

1b30bb882d4e3132c76cd2a0492e457faa0cdf10
Antoine Delahaye <antoine.delahaye24.01@gmail.com>
2021-03-18 17:09:40 +0100

    Fix movies display

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,7 +25,7 @@ export class HomeComponent implements OnInit {
       .getListOfGroup(`https://api.themoviedb.org/3/movie/popular?api_key=ccbc42c4b357545c785bb0d1caba6301&language=${this.translateService.currentLang}&page=1`)
       .subscribe(
         data => {
-          for (const movie of data.results) {
+          for (const movie of data.results.slice(0, 10)) {
             this.movies.push(plainToClass(Movie, movie));
           }
         },

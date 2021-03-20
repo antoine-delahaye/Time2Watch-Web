03d4782b6b169d9c099caa8b119485bb1e06f2c2
Antoine Delahaye <antoine.delahaye24.01@gmail.com>
2021-03-20 10:32:11 +0100

    Add pagination

--- src/app/tvshows/tvshows.component.ts
+++ src/app/tvshows/tvshows.component.ts
@@ -3,6 +3,8 @@ import {plainToClass} from 'class-transformer';
 import {Service} from '../service';
 import {TranslateService} from '@ngx-translate/core';
 import {TVShows} from './tvshows';
+import {ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-tvshows',
@@ -14,19 +16,26 @@ export class TvshowsComponent implements OnInit {
 
   tvshows: TVShows[];
 
-  constructor(private service: Service, private translateService: TranslateService) {
+  page: number;
+
+  constructor(private route: ActivatedRoute, private service: Service, private translateService: TranslateService) {
   }
+
+  private routeSub: Subscription;
 
   @HostBinding('class') class = 'my-auto col-12';
 
   ngOnInit(): void {
-    this.getPopularTVShows();
+    this.routeSub = this.route.params.subscribe(params => {
+      this.page = params.page;
+      this.getPopularTVShows();
+    });
   }
 
   getPopularTVShows(): any {
     this.tvshows = [];
     this.service
-      .getListOfGroup(`https://api.themoviedb.org/3/tv/popular?api_key=ccbc42c4b357545c785bb0d1caba6301&language=${this.translateService.currentLang}&page=1`)
+      .getListOfGroup(`https://api.themoviedb.org/3/tv/popular?api_key=ccbc42c4b357545c785bb0d1caba6301&language=${this.translateService.currentLang}&page=${this.page}`)
       .subscribe(
         data => {
           for (const tvshow of data.results) {
